123f2734c5cb812d15274f0e6939cfc164f3195e
Jeffy Yu <105130385+yu-jeffy@users.noreply.github.com>
2024-03-13 00:49:29 +0000

    favicon, styling, scaffold gallery

--- pages/gallery.js
+++ pages/gallery.js
@@ -13,53 +13,52 @@ function Gallery() {
         if (view === 'grid') {
             return (
                 <div className={styles.gridContainer}>
-                    <Image src="/path/to/image1.jpg" width={300} height={200} />
-                    <Image src="/path/to/image2.jpg" width={300} height={200} />
-                    <Image src="/path/to/image3.jpg" width={300} height={200} />
-                    <Image src="/path/to/image1.jpg" width={300} height={200} />
-                    <Image src="/path/to/image2.jpg" width={300} height={200} />
-                    <Image src="/path/to/image3.jpg" width={300} height={200} />
-                    <Image src="/path/to/image1.jpg" width={300} height={200} />
-                    <Image src="/path/to/image2.jpg" width={300} height={200} />
-                    <Image src="/path/to/image3.jpg" width={300} height={200} />
-                    <Image src="/path/to/image1.jpg" width={300} height={200} />
-                    <Image src="/path/to/image2.jpg" width={300} height={200} />
-                    <Image src="/path/to/image3.jpg" width={300} height={200} />
+                    <Image className = {styles.gridArt} src="/art/plastic.jpg" width={150} height={250} />
+                    <Image className = {styles.gridArt} src="/art/plastic.jpg" width={150} height={250} />
+                    <Image className = {styles.gridArt} src="/art/plastic.jpg" width={150} height={250} />
+                    <Image className = {styles.gridArt} src="/art/plastic.jpg" width={150} height={250} />
+                    <Image className = {styles.gridArt} src="/art/plastic.jpg" width={150} height={250} />
+                    <Image className = {styles.gridArt} src="/art/plastic.jpg" width={150} height={250} />
+                    <Image className = {styles.gridArt} src="/art/plastic.jpg" width={150} height={250} />
+                    <Image className = {styles.gridArt} src="/art/plastic.jpg" width={150} height={250} />
+                    
                 </div>
             );
         } else {
             return (
-                <div className={styles.listContainer}>
-                    {/* Add your list view content here */}
-                    <div className={styles.listItem}>
-                        <Image src="/path/to/image1.jpg" width={100} height={100} />
-                        <div className={styles.itemDetails}>
-                            <h3>Art Piece 1</h3>
-                            <p>Description of art piece 1</p>
+                    <div className={styles.listContainer}>
+                        {/* Add your list view content here */}
+                        <div className={styles.listItem}>
+                            <Image src="/art/plastic.jpg" width={150} height={250} />
+                            <div className={styles.itemDetails}>
+                                <h3>Art Piece 1</h3>
+                                <p>Description of art piece 1</p>
+                            </div>
+                        </div>
+                        <div className={styles.listItem}>
+                            <Image src="/art/plastic.jpg" width={150} height={250} />
+                            <div className={styles.itemDetails}>
+                                <h3>Art Piece 2</h3>
+                                <p>Description of art piece 2</p>
+                            </div>
+                        </div>
+                        <div className={styles.listItem}>
+                            <Image src="/art/plastic.jpg" width={150} height={250} />
+                            <div className={styles.itemDetails}>
+                                <h3>Art Piece 3</h3>
+                                <p>Description of art piece 3</p>
+                            </div>
                         </div>
                     </div>
-                    <div className={styles.listItem}>
-                        <Image src="/path/to/image2.jpg" width={100} height={100} />
-                        <div className={styles.itemDetails}>
-                            <h3>Art Piece 2</h3>
-                            <p>Description of art piece 2</p>
-                        </div>
-                    </div>
-                    <div className={styles.listItem}>
-                        <Image src="/path/to/image3.jpg" width={100} height={100} />
-                        <div className={styles.itemDetails}>
-                            <h3>Art Piece 3</h3>
-                            <p>Description of art piece 3</p>
-                        </div>
-                    </div>
-                </div>
             );
         }
     };
 
     return (
-        <div>
-            <button onClick={toggleView}>Switch View</button>
+        <div className={styles.container}>
+            <button onClick={toggleView} style={{ fontFamily: 'Arial', fontSize: '3rem' }}>
+                {view === 'grid' ? '\u2630' : '\u1392\u1392\u1392'}
+            </button>
             {renderGallery()}
         </div>
     );
