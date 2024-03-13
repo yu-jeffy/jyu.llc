311472aecb4e47454d8fbc704bc4246baca6ca26
Jeffy Yu <105130385+yu-jeffy@users.noreply.github.com>
2024-03-13 00:57:35 +0000

    home only on other pages

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,14 +1,21 @@
 import Link from 'next/link';
 import styles from '../styles/NavBar.module.css';
+import { useRouter } from 'next/router';
 
 const NavBar = () => {
+    const router = useRouter();
+
     return (
         <nav className={styles.nav}>
             <ul className={styles.navList}>
+                {router.pathname !== '/' && (
+                    <li className={styles.navItem}>
+                        <Link href="/" className={styles.navLink}>
+                            home
+                        </Link>
+                    </li>
+                )}
                 <li className={styles.navItem}>
-                    <Link href="/" className={styles.navLink}>
-                        home
-                    </Link>
                     <Link href="/portfolio" className={styles.navLink}>
                         technical portfolio
                     </Link>
